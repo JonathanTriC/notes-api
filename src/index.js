9e4d53a83c232db03eac27f5be3b557042c51a6e
Jonathan Tri <jonathantric@gmail.com>
2023-05-30 13:59:55 +0700

    add update and delete request

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 
+// GET
 app.get("/notes", async (req, res) => {
 	try {
 		const notes = await Note.find({});
@@ -19,6 +20,7 @@ app.get("/notes", async (req, res) => {
 	}
 });
 
+// POST
 app.post("/notes", async (req, res) => {
 	const note = new Note(req.body);
 
@@ -28,6 +30,41 @@ app.post("/notes", async (req, res) => {
 		res.status(200).send(note);
 	} catch (error) {
 		console.log("Error post: ", error);
+		res.status(500).send(error);
+	}
+});
+
+// UPDATE
+app.patch("/notes/:id", async (req, res) => {
+	try {
+		const note = await Note.findById(req.params.id);
+
+		if (!note) {
+			return res.status(404).send();
+		}
+
+		note.note = req.body.note;
+		await note.save();
+
+		res.status(200).send(note);
+	} catch (error) {
+		console.log("Error update: ", error);
+		res.status(404).send(error);
+	}
+});
+
+// DELETE
+app.delete("/notes/:id", async (req, res) => {
+	try {
+		const note = await Note.findByIdAndDelete(req.params.id);
+
+		if (!note) {
+			return res.status(404).send();
+		}
+
+		res.status(200).send("The note has been deleted");
+	} catch (error) {
+		console.log("Error delete: ", error);
 		res.status(500).send(error);
 	}
 });
